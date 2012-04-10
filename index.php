4d2ab47d24729537e17458e4535a9d5c30259cfe
Pablo Vicente <vicapa99@gmail.com>
2012-04-10 11:49:06 +0100

    Add google analitics

--- index.php
+++ index.php
@@ -12,6 +12,19 @@
 	<?php } ?>
 
    	<link href='http://fonts.googleapis.com/css?family=Exo:400,300&subset=latin-ext,latin' rel='stylesheet' type='text/css'>
+    <script type="text/javascript">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-12123003-2']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    </script>
 </head>
 <body>
 	<div id="company" class="centered">
